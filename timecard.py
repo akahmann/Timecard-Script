20cea7bd4f095aecc6bf7b6f93ad1cdbdc725707
akahmann <akahmann93@gmail.com>
2021-04-23 11:26:35 -0600

    Find previous time

--- timecard.py
+++ timecard.py
@@ -1,7 +1,7 @@
-import datetime
+from datetime import datetime
 import re
 
-x = datetime.datetime.now()
+x = datetime.now()
 
 #get date and time
 monthDayYear = x.strftime("%x")
@@ -21,25 +21,21 @@ if len(lineList) > 0:
 		print("Logout - Ending work")
 		f = open("timecard.txt", "a")
 		f.write("LOGOUT " + curTime + " - ")
-		#TODO find previous time
-		prevTime = "10:33:21"
+		#find previous time
+		prevLine = lineList[len(lineList) - 2]
+		prevTime = re.search("\d\d:\d\d:\d\d", prevLine)
+		prevTime = str(prevTime)
+		prevTime = prevTime[-10:-2]
 		fmt = "%H:%M:%S"
-		timeSpent = datetime.datetime.strptime(curTime, fmt) - datetime.datetime.strptime(prevTime, fmt)
+		timeSpent = datetime.strptime(curTime, fmt) - datetime.strptime(prevTime, fmt)
 		timeSpent = str(timeSpent)
 		f.write(timeSpent + "\n")
 		f.close()
+		#show time spent
 		print("Time spent - " + timeSpent)
-		#display
 #else assume they are logging in
 else:
 	print("Login - Starting work")
 	f = open("timecard.txt", "a")
 	f.write("LOGIN " + curTime + "\n")
 	f.close()
-
-# txt = "lalala alladsf 09:20 dfdsafsd"
-
-# y = re.search("\d\d:\d\d", txt)
-# print(y)
-# z = re.split("\D", y)
-# print(z)
